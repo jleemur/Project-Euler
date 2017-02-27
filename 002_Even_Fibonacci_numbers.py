41726e3ef5019ebae16c29da0f0af8eef0bf24c1
Jonathan Murray <jon.lee.murray@gmail.com>
2017-02-26 23:05:29 -0400

    up -> upper... ocd commit

--- 002_Even_Fibonacci_numbers.py
+++ 002_Even_Fibonacci_numbers.py
@@ -1,10 +1,10 @@
-# Recursive method to count all even fibonacci values below :upLimit:
-def getEvenFibonacciSum(prev, curr, sum, upLimit):
+# Recursive method to count all even fibonacci values below :upperLimit:
+def getEvenFibonacciSum(prev, curr, sum, upperLimit):
     if curr%2==0:
         sum += curr
-    if prev+curr > upLimit:
+    if prev+curr > upperLimit:
         return sum
-    return getEvenFibonacciSum(curr, prev+curr, sum, upLimit)
+    return getEvenFibonacciSum(curr, prev+curr, sum, upperLimit)
 
 if __name__ == "__main__":
     print(getEvenFibonacciSum(1,2,0,4000000))
