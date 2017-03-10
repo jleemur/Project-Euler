28d98a9df27a29818e50794bc43547b00bf66943
JLeemur <jon.lee.murray@gmail.com>
2017-03-09 23:42:43 -0400

    Complete. Not my most creative approach...

---
+++ 013_Large_sum.py
@@ -0,0 +1,16 @@
+# Returns the 10 left-most digits of the sum of n_arr
+def get_large_sum(n_arr):
+    sum = 0L
+    for i in n_arr:
+        sum += i
+    return str(sum)[:10]
+
+if __name__ == "__main__":
+    n_arr = []
+
+    # 100 50-digit numbers
+    f = open('013_Large_sum.txt', 'r')
+    for line in f:
+        n_arr.append(int(line))
+
+    print(get_large_sum(n_arr))
